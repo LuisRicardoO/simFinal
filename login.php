f7873cb5f00fba9b904554d62f2d60a2c589312c
LAPTOP-4BIQSEC7\fbdia <fb.dias@campus.fct.unl.pt>
2017-11-21 15:21:11 +0000

    tens ai

---
+++ login.php
@@ -0,0 +1,38 @@
+<html>
+<TABLE border="1" width="80%" align="center">
+	<TR>
+		<TH colspan='2'> CLINICA SIM-FCT
+		<a href="https://www.fct.unl.pt/" target="_blank"><img src="logo.png" align="left"></a>
+		</TH>
+		
+	</TR>
+	<TR>
+		<TD valign="top">
+			<TABLE border="0">
+				<TR>
+					<TD bgcolor="#00F000">Opções</TD>
+				</TR>
+				<TR>
+					<TD>  <a href="index.php" target="_self">Apresentação</a></TD>
+				</TR>
+				<TR>
+					<TD> <a href="login.php" target="_self">Login</a></TD>
+				</TR>				
+
+			</TABLE>
+		
+		</TD>
+		<TD align="center"> 
+			<form method="POST" action="login.php">
+			Utilizador: <input type="text" name="user" >
+			<p>Password: <input type="password" name="pass" ></p>
+			<input type="submit" name="Submit" value="Submit" >
+			</form>
+		</TD>
+	</TR>
+	<TR>
+	<TD bgcolor="#F0F0F0" align="center" colspan='2'>@Alunos 2016/17</TD>
+	</TR>
+
+</TABLE>
+</html>
