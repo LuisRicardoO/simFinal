50055c5f8bf26ed2f703c8dbb7091876f4c24c24
Ricardo <33837463+Ricardo@users.noreply.github.com>
2017-11-21 16:10:36 +0000

    actualizei index e login (12.11.2017)

--- index.php
+++ index.php
@@ -15,6 +15,47 @@
 			case "home":
 				$todo="goHome";
 				break;
+			case "login":
+				$todo="goLogin";
+				break;
+			case "checkLogin":
+				if(isset($_POST['submit'])){	
+						$password= ($_POST['pass']);																	
+						$sql = 'SELECT username, pass, type  FROM USER WHERE (username = "'.$_POST['user'].'" AND pass = "'.$password. '")';
+						//echo "$sql";
+						$result = mysqli_query ($link ,$sql)
+							or die('The query failed: ' . mysqli_error($link));						
+						$number = mysqli_num_rows($result); //if it returns 1 it is a valid user
+
+						echo "aqui esta o numero: ".$number;
+
+						if(isset($number)){					
+							if($number==1){
+								$_SESSION['started']='true';
+								$_SESSION['user']=$_POST['user'];
+								$row = mysqli_fetch_array($result);							
+								if(isset($row['type'])){
+									$type=$row['type'];
+									switch($type):
+										case 1:
+											$todo="goResearch";							
+											break;
+										case 2:
+											$todo="goClient";
+											break;
+										case 3:
+											$todo="goNutritionist";
+											break;
+										default:
+											$todo="goUserNotvalid";
+									endswitch;
+								}								
+							}else{
+								$todo="goWrongLogin";													
+							}
+						}						
+					}
+				break;
 			default:
 				$todo="goHome";
 		endswitch;
@@ -28,10 +69,10 @@
 				NutriNatura
 			</th>
 			<td width=10% align="center" valign="center">
-				Log in
+				<a href="http://localhost/trabFinal/repo/index.php?operation=login"> Sign in </a>
 			</td>
 			<td width=10% align="center" valign="center">
-				Sign in
+				Sign up
 			</td>		
 		</tr> 
 		<tr height=2.5%>
@@ -47,6 +88,24 @@
 							case "goHome":
 								include "home.php";
 								break;
+							case "goLogin":
+								include "login.php";
+								break;
+							case "goResearch":
+								echo "welcome reseacher";
+								break;
+							case "goClient":
+								echo "welcome client";
+								break;
+							case "goNutritionist":
+								echo "welcome nutritionist";
+								break;
+							case "goUserNotvalid":
+								echo "user not valid has to type";
+								break;		
+							case "goWrongLogin":
+								echo "not a valid user";
+								break;					
 							default:
 								include "home.php";
 						endswitch;						
