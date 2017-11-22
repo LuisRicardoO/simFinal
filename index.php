40ad0a178891fd1f142562c681adc23bf95d71d4
Ricardo <33837463+Ricardo@users.noreply.github.com>
2017-11-22 11:46:30 +0000

    ibu

--- index.php
+++ index.php
@@ -1,121 +1,135 @@
 <html>
 <head>
-<title>NutriNatura</title>
+    <title>NutriNatura</title>
 </head>
 
 <!--preprocessamento-->
 <?php
-	//start
-	session_start();
-	//connect to data base
-	$link = mysqli_connect('localhost', 'root', '','SIM2')or die('Error connecting to the server: ' . mysqli_error($link));
-	
-	if(isset($_GET['operation'])){
-		switch ($_GET['operation']):
-			case "home":
-				$todo="goHome";
-				break;
-			case "login":
-				$todo="goLogin";
-				break;
-			case "checkLogin":
-				if(isset($_POST['submit'])){	
-						$password= ($_POST['pass']);																	
-						$sql = 'SELECT username, pass, type  FROM USER WHERE (username = "'.$_POST['user'].'" AND pass = "'.$password. '")';
-						//echo "$sql";
-						$result = mysqli_query ($link ,$sql)
-							or die('The query failed: ' . mysqli_error($link));						
-						$number = mysqli_num_rows($result); //if it returns 1 it is a valid user
+//start
+session_start();
+//connect to data base
+$link = mysqli_connect('localhost', 'root', '', 'SIM2') or die('Error connecting to the server: ' . mysqli_error($link));
 
-						echo "aqui esta o numero: ".$number;
+if (isset($_GET['operation'])) {
+    switch ($_GET['operation']):
+        case "home":
+            $todo = "goHome";
+            break;
+        case "login":
+            $todo = "goLogin";
+            break;
+        case "checkLogin":
+            if (isset($_POST['submit'])) {
+                $password = ($_POST['pass']);
+                $sql = 'SELECT username, pass, type  FROM USER WHERE (username = "' . $_POST['user'] . '" AND pass = "' . $password . '")';
+                //echo "$sql";
+                $result = mysqli_query($link, $sql)
+                or die('The query failed: ' . mysqli_error($link));
+                $number = mysqli_num_rows($result); //if it returns 1 it is a valid user
+                if (isset($number)) {
+                    if ($number == 1) {
+                        $row = mysqli_fetch_array($result);
+                        if (isset($row['type'])) {
+                            $type = $row['type'];
+                            if ($type == 1 || $type == 2 || $type == 3) {
+                                $todo = "goSession";
+                                $_SESSION['started'] = 'true';
+                                $_SESSION['user'] = $_POST['user'];
+                                $_SESSION['type'] = $type;
+                            } else {
+                                $todo = "goUserNotValid";
+                            }
+                        }
+                    }
+                } else {
+                    $todo = "goWrongLogin";
+                }
+            }
 
-						if(isset($number)){					
-							if($number==1){
-								$_SESSION['started']='true';
-								$_SESSION['user']=$_POST['user'];
-								$row = mysqli_fetch_array($result);							
-								if(isset($row['type'])){
-									$type=$row['type'];
-									switch($type):
-										case 1:
-											$todo="goResearch";							
-											break;
-										case 2:
-											$todo="goClient";
-											break;
-										case 3:
-											$todo="goNutritionist";
-											break;
-										default:
-											$todo="goUserNotvalid";
-									endswitch;
-								}								
-							}else{
-								$todo="goWrongLogin";													
-							}
-						}						
-					}
-				break;
-			default:
-				$todo="goHome";
-		endswitch;
-	}
+            break;
+        case "logOut":
+            session_unset();
+            session_destroy();
+            $todo = "goLogOut";
+            $todoStatedSession = false;
+            break;
+        default:
+            $todo = "goHome";
+    endswitch;
+}
 ?>
 
 <body>
-	<table border="1" height=100% width=100% align="center" valign="center">
-		<tr height=5%>
-			<th width=80% align="center" valign="center">
-				NutriNatura
-			</th>
-			<td width=10% align="center" valign="center">
-				<a href="http://localhost/trabFinal/repo/index.php?operation=login"> Sign in </a>
-			</td>
-			<td width=10% align="center" valign="center">
-				Sign up
-			</td>		
-		</tr> 
-		<tr height=2.5%>
-			<td colspan="3">
-			
-			</td>	
-		</tr>
-		<tr>				
-			<td colspan="3"><!--insert here the content of site-->
-				<?php	
-					if(isset($todo)){
-						switch ($todo):
-							case "goHome":
-								include "home.php";
-								break;
-							case "goLogin":
-								include "login.php";
-								break;
-							case "goResearch":
-								echo "welcome reseacher";
-								break;
-							case "goClient":
-								echo "welcome client";
-								break;
-							case "goNutritionist":
-								echo "welcome nutritionist";
-								break;
-							case "goUserNotvalid":
-								echo "user not valid has to type";
-								break;		
-							case "goWrongLogin":
-								echo "not a valid user";
-								break;					
-							default:
-								include "home.php";
-						endswitch;						
-					}else{
-						include "home.php";
-					}
-				?>					
-			</td>
-		</tr> 
-	</table>
+<table border="0" height=100% width=100% align="center" valign="center">
+    <tr height=5%>
+        <th width=80% align="center" valign="center" bgcolor="#add8e6">
+            <a href="index.php?opoeration=home"> NutriNatura</a>
+        </th>
+        <td width=10% align="center" valign="center" bgcolor="#afeeee">
+
+            <?php
+            if (isset($_SESSION['started']) && ($_SESSION['started'] == true)) {
+                echo '<a href = "index.php?operation=logOut"> Sign out</a >';
+            } else {
+                echo '<a href = "index.php?operation=login"> Sign in</a >';
+            }
+            ?>
+        </td>
+        <td width=10% align="center" valign="center" bgcolor="#5f9ea0">
+            Sign up
+        </td>
+    </tr>
+    <!--<tr height=2.5%>
+        <td colspan="3">
+
+        </td>
+    </tr>-->
+    <tr>
+        <td colspan="3"><!--insert here the content of site-->
+            <?php
+            if (isset($todo)) {
+                switch ($todo):
+                    case "goHome":
+                        include "home.php";
+                        break;
+                    case "goLogin":
+                        $wrongLogin = false;
+                        include "login.php";
+                        break;
+                    case "goWrongLogin":
+                        //echo "not a valid user";
+                        $wrongLogin = true;
+                        include "login.php";
+                        break;
+                    case "goLogOut":
+                        echo "see you next time";
+                        break;
+                endswitch;
+                //subcontent
+                if (isset($_SESSION['started']) && ($_SESSION['started'] == true)) {
+
+                    switch ($_SESSION['type']):
+                        case 1://reseacher
+                            echo "welcome reseacher";
+                            break;
+                        case 2://client
+                            echo "welcome client";
+                            break;
+                        case 3://nutritionist
+                            //echo "welcome nutritionist";
+                            include "nutritionist.php";
+                            break;
+                        default:
+                            echo "u should never arrive here";
+                    endswitch;
+                }
+            } else {
+                include "home.php";
+            }
+            ?>
+        </td>
+    </tr>
+</table>
 </body>
 
 
--- login.php
+++ login.php
@@ -1,7 +1,16 @@
 <html>
+
+
+
+
 <body>
 	<form method="POST" action="http://localhost/trabFinal/repo/index.php?operation=checkLogin">
-		<p>Username: <input type="text" name="user" ></p>
+		<p>Username: <input type="text" name="user" >
+        <?php
+           if($wrongLogin)
+               echo " wrong username. Try again";
+        ?>
+        </p>
 		<p>Password: <input type="password" name="pass" ></p>
 		<p><input type="submit" name="submit" value="Submit" ></p>
 	</form>	
